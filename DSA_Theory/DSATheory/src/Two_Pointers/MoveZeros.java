2818c3f7af504c65bcac80892cf91fa4015a2feb
Trần Xuân Hoàn (K18 HL) <hoantxhe180030@fpt.edu.vn>
2025-02-15 11:09:21 +0700

    add theory

--- DSA_Theory/DSATheory/src/Two_Pointers/MoveZeros.java
+++ DSA_Theory/DSATheory/src/Two_Pointers/MoveZeros.java
@@ -3,15 +3,8 @@ package Two_Pointers;
 import java.util.Arrays;
 
 public class MoveZeros {
-    public static void main(String[] args) {
-//        int[] nums = {0, 1, 0, 3, 12};
-//        int[] nums = {1, 3, 4, 5, 6};
-        int[] nums = {0, 0, 0, 1};
-        moveZeroes(nums);
-        System.out.println(Arrays.toString(nums));
-    }
-
     /**
+     * <b>Kỹ thuật Move Zeros</b>
      * <ol>
      *     <li>Đặt 2 con trỏ <b>left</b> và <b>right</b>. Tạm thời cho <b>left</b> chiếm index = 0.</li>
      *     <li>Cho mảng chạy với chỉ số của <b>right</b>.</li>
@@ -19,7 +12,6 @@ public class MoveZeros {
      *     <li>Nếu giá trị tại chỉ số của <b>right</b> khác 0, ta hoán đổi cho <b>left</b>, đồng thời tăng <b>left</b> lên 1 đơn vị (có nghĩa là <b>left</b> chỉ dịch lên khi gtr tại chỉ số <b>right</b> khác 0, còn lại <b>left</b> giữ nguyên vị trí)</li>
      *     <li>Dễ hiểu hơn, nếu gtr tại <b>right</b> khác 0, thì việc đổi chỗ nums[<b>right</b>] và nums[<b>left</b>] giống như gtr tại <b>right</b> hoán vị tại chỗ, còn 1 khi khác 0, vị trí của 0 mà <b>left</b> đang nắm thế chỗ cho vị trí cuả <b>right</b></li>
      * </ol>
-     * @param nums
      */
     private static void moveZeroes(int[] nums) {
         int left = 0;
@@ -35,5 +27,13 @@ public class MoveZeros {
             }
             System.out.println("After moving: " + Arrays.toString(nums));
         }
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = {0, 1, 0, 3, 12};
+//        int[] nums = {1, 3, 4, 5, 6};
+        int[] nums = {0, 0, 0, 1};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
